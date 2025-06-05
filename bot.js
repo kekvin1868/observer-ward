d4dae4202fc9aee00736e4dd83adccd88b80349f
Kevin Hartono <kevinhartono2810@gmail.com>
2025-06-05 19:44:27 +0700

    feat(Bot): test accept user messages

--- bot.js
+++ bot.js
@@ -1,8 +1,18 @@
 import 'dotenv/config';
-import { login } from './services/bluesky/client.js';
+import { login as loginBsky } from './services/bluesky/client.js';
+import { discordClient } from './services/discord/client.js';
 
 async function startBot() {
-  await login();
+  await loginBsky();
+
+  discordClient.on('messageCreate', async message => {
+    // Ignore bot messages
+    if (message.author.bot) return;
+
+    if (message.content.startsWith('/hello')) {
+      message.reply('Hello there! I am online and listening.');
+    }
+  });
 
   console.log('🤖 Bot is up and running!');
 }
